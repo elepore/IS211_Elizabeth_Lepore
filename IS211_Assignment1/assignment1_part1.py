e06ddfaee321c005d2b95487f6c1bc84051a282d
elepore <52303156+elepore@users.noreply.github.com>
2023-09-04 23:20:56 -0400

    All working, move folder

--- IS211_Assignment1/assignment1_part1.py
+++ IS211_Assignment1/assignment1_part1.py
@@ -1,19 +1,48 @@
 
-def list_divide(numbers, divide):
+# Note: underscores added to name, different from https://github.com/cubanquant/IS211_Assignment1/blob/master/assignment1_part1.py
+def list_divide(numbers, divide=2):
     """
-    The function returns the number of elements in the numbers list that are divisibleby divide
+    The function returns the number of elements in the numbers list that are divisible by divide
     """
+    return len([x for x in numbers if x % divide == 0])
+    # List comprehension, for numbers in number goes through each number (x), 
+    # if number (x) % divided == 0, it means it's divisble. This list comprehension will 
+    # make a new list of those that are divisible, then we use len() to count.  
+
+class ListDivideException(Exception):
     pass
+
 
 def test_list_divide():
     """
-    Test listDivide
+    Test list_divide
     """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
+    try:
+        assert list_divide([1,2,3,4,5]) == 2
+    except AssertionError:
+        raise ListDivideException("Test Failed") 
+        # Wasn't sure if you wanted us to incorporate the exception 
     
+    try:
+        assert list_divide([2,4,6,8,10]) == 5
+    except AssertionError:
+        raise ListDivideException("Test Failed")
+    
+    try:
+        assert list_divide([30, 54, 63,98, 100], divide=10) == 2
+    except AssertionError:
+        raise ListDivideException("Test Failed")
+    
+    try:
+        assert list_divide([]) == 0
+    except AssertionError:
+        raise ListDivideException("Test Failed")
+    
+    try:
+        assert list_divide([1,2,3,4,5], 1) == 5
+    except AssertionError:
+        raise ListDivideException("Test Failed")
+
 if __name__ == "__main__":
-    testListDivide()
+    test_list_divide()
+
