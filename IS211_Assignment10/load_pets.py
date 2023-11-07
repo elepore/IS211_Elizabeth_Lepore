6ec1f3b1b69fee6bc14b2156ca0031feb4e03bc4
elepore <52303156+elepore@users.noreply.github.com>
2023-11-06 22:45:13 -0500

    Assignment 10

--- IS211_Assignment10/load_pets.py
+++ IS211_Assignment10/load_pets.py
@@ -1,3 +1,41 @@
 
+import sqlite3
+
+def load_data(db_path):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    persons = [
+        (1, 'James', 'Smith', 41),
+        (2, 'Diana', 'Greene', 23),
+        (3, 'Sara', 'White', 27),
+        (4, 'William', 'Gibson', 23)
+    ]
+
+    pets = [
+        (1, 'Rusty', 'Dalmation', 4, 1),
+        (2, 'Bella', 'Alaskan Malamute', 3, 0),
+        (3, 'Max', 'Cocker Spaniel', 1, 0),
+        (4, 'Rocky', 'Beagle', 7, 0),
+        (5, 'Rufus', 'Cocker Spaniel', 1, 0),
+        (6, 'Spot', 'Bloodhound', 2, 1)
+    ]
+
+    person_pets = [
+        (1, 1),
+        (1, 2),
+        (2, 3),
+        (2, 4),
+        (3, 5),
+        (4, 6)
+    ]
+
+    cursor.executemany("INSERT INTO person VALUES (?, ?, ?, ?)", persons)
+    cursor.executemany("INSERT INTO pet VALUES (?, ?, ?, ?, ?)", pets)
+    cursor.executemany("INSERT INTO person_pet VALUES (?, ?)", person_pets)
+    conn.commit()
+    conn.close()
+
 if __name__ == "__main__":
-    pass
+    db_path = "pets.db"
+    load_data(db_path)
--- IS211_Assignment10/query_pets.py
+++ IS211_Assignment10/query_pets.py
@@ -1,3 +1,51 @@
 
+import sqlite3
+
+def query_data(db_path):
+    # Connect to the SQLite database
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    while True:
+        # Ask the user for a person's ID number
+        person_id = input("Please enter a person's ID number or '-1' to exit: ")
+        if person_id == '-1':
+            break
+
+        try:
+            person_id = int(person_id)
+        except ValueError:
+            print("ID must be a number.")
+            continue
+
+        # Query the database for the person and their pets
+        cursor.execute("SELECT * FROM person WHERE id = ?", (person_id,))
+        person_data = cursor.fetchone()
+
+        if person_data:
+            first_name, last_name, age = person_data[1:4]
+            print(f"{first_name} {last_name}, {age} years old")
+
+            # Query for the person's pets
+            cursor.execute('''
+            SELECT pet.name, pet.breed, pet.age, pet.dead
+            FROM pet
+            INNER JOIN person_pet ON pet.id = person_pet.pet_id
+            WHERE person_pet.person_id = ?
+            ''', (person_id,))
+            pets_data = cursor.fetchall()
+
+            for pet in pets_data:
+                name, breed, age, dead = pet
+                status = "dead" if dead else "alive"
+                print(f"{first_name} {last_name} owned {name}, a {breed}, that was {age} years old ({status})")
+        else:
+            print("Person not found.")
+
+    # Close the connection to the database
+    conn.close()
+
 if __name__ == "__main__":
-    pass
+    # Path to the SQLite database
+    db_path = "pets.db"
+    query_data(db_path)
